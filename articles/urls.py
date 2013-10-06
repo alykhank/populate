2423c7b47535a857a9b3b96cc8d40fd7bd961038
Alykhan Kanji <github@alykhan.com>
2013-10-06 05:12:35 -0400

    Complete Readability authorization

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,6 @@ from articles import views
 urlpatterns = patterns('',
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^auth/$', views.auth, name='auth'),
+	url(r'^submit/$', views.submit, name='submit'),
+	url(r'^bookmarks/(?P<request_token>\w+)/(?P<request_token_secret>\w+)/(?P<oauth_verifier>\w+)$', views.bookmarks, name='bookmarks'),
 )
--- articles/views.py
+++ articles/views.py
@@ -7,31 +7,39 @@ from rauth import OAuth1Service
 
 from articles.models import Bookmark, Article
 
+readability = OAuth1Service(
+	consumer_key=os.environ.get('CONS_KEY'),
+	consumer_secret=os.environ.get('CONS_SECRET'),
+	name='readability',
+	request_token_url='https://www.readability.com/api/rest/v1/oauth/request_token/',
+	authorize_url='https://www.readability.com/api/rest/v1/oauth/authorize/',
+	access_token_url='https://www.readability.com/api/rest/v1/oauth/access_token/',
+	base_url='https://www.readability.com/api/rest/v1/'
+)
+
 class IndexView(generic.ListView):
 	template_name = 'articles/index.html'
 
 	def get_queryset(self):
 		return Bookmark.objects.all()
 
-	def get_context_data(self, **kwargs):
-		# Call the base implementation first to get a context
-		context = super(IndexView, self).get_context_data(**kwargs)
-		# Add in a QuerySet of all the genres
-		context['auth_url'] = reverse('articles:auth')
-		return context
-
 def auth(request):
-	readability = OAuth1Service(
-		consumer_key=os.environ.get('CONS_KEY'),
-		consumer_secret=os.environ.get('CONS_SECRET'),
-		name='readability',
-		request_token_url='https://www.readability.com/api/rest/v1/oauth/request_token/',
-		authorize_url='https://www.readability.com/api/rest/v1/oauth/authorize/',
-		access_token_url='https://www.readability.com/api/rest/v1/oauth/access_token/',
-		base_url='https://www.readability.com/api/rest/v1/'
-	)
-
 	redirect_uri = 'http://populate-mit.herokuapp.com/auth'
 	request_token, request_token_secret = readability.get_request_token()
 	url = readability.get_authorize_url(request_token, data={'oauth_callback': redirect_uri})
-	return HttpResponseRedirect(url)
+	return render(request, 'articles/auth.html', {'request_token': request_token, 'request_token_secret': request_token_secret, 'oauth_url': url})
+
+def submit(request):
+	try:
+		request_token = request.POST['request_token']
+		request_token_secret = request.POST['request_token_secret']
+		oauth_verifier = request.POST['verification']
+	except (KeyError):
+		return render(request, 'articles/index.html')
+	else:
+		return HttpResponseRedirect(reverse('articles:bookmarks', kwargs={'request_token': request_token, 'request_token_secret': request_token_secret, 'oauth_verifier': oauth_verifier}))
+
+def bookmarks(request, request_token, request_token_secret, oauth_verifier):
+	session = readability.get_auth_session(request_token, request_token_secret, data={'oauth_verifier': oauth_verifier})
+	return HttpResponse(session.get('bookmarks'))
+
