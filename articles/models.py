53820083645899cf4a32201b804016034007ab9a
Alykhan Kanji <github@alykhan.com>
2013-10-06 02:28:16 -0400

    Write models for articles app

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+	domain = models.CharField(max_length=200)
+	# next_page_href
+	author = models.CharField(max_length=200)
+	url = models.URLField()
+	lead_image_url = models.URLField()
+	# content_size
+	title = models.CharField(max_length=200)
+	excerpt = models.CharField(max_length=200)
+	word_count = models.PositiveIntegerField(default=0)
+	content = models.TextField()
+	date_published = models.DateTimeField()
+	dek = models.CharField(max_length=200)
+	# processed
+	short_url = models.URLField()
+	# article_id
+
+	def __unicode__(self):
+		return "<Article('%s', '%s')>" % (self.title, self.author)
+
+
+class Bookmark(models.Model):
+	user_id = models.PositiveIntegerField(default=0)
+	read_percent = models.FloatField(default=0.0)
+	date_updated = models.DateTimeField()
+	favorite = models.BooleanField()
+	article = models.ForeignKey(Article)
+	bookmark_id = models.PositiveIntegerField(default=0)
+	date_archived = models.DateTimeField()
+	date_opened = models.DateTimeField()
+	date_added = models.DateTimeField()
+	article_href = models.CharField(max_length=200)
+	date_favorited = models.DateTimeField()
+	archive = models.BooleanField()
+	# tags
+
+	def __unicode__(self):
+		return "<Bookmark('%s', '%s')>" % (self.bookmark_id, self.article.title)
