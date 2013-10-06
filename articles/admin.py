c799aa8cd3367daeed4680debc2bd49847f51d84
Alykhan Kanji <github@alykhan.com>
2013-10-06 03:02:44 -0400

    Rearrange admin panels

--- articles/admin.py
+++ articles/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
-from articles.models import Article, Bookmark
+from articles.models import Bookmark, Article
 
 class ArticleInline(admin.TabularInline):
 	model = Article
 
-class ArticleAdmin(admin.ModelAdmin):
-	list_display = ('title', 'author', 'excerpt', 'dek', 'word_count', 'date_published', 'url')
-
 class BookmarkAdmin(admin.ModelAdmin):
 	inlines = [ArticleInline]
 
-admin.site.register(Article, ArticleAdmin)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'excerpt', 'dek', 'word_count', 'date_published', 'url')
+
 admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Article, ArticleAdmin)
