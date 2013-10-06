b6e17f8fda047c25b87b4863e7c46e47c4cc058a
Alykhan Kanji <github@alykhan.com>
2013-10-05 21:11:00 -0400

    Configure database, staticfiles

--- populate/settings.py
+++ populate/settings.py
@@ -1,10 +1,13 @@
 # Django settings for populate project.
 
-DEBUG = True
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Alykhan Kanji', 'github@alykhan.com'),
+    ('Kevin Lau', 'kevinlau@live.com'),
 )
 
 MANAGERS = ADMINS
@@ -156,3 +159,21 @@ LOGGING = {
         },
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
